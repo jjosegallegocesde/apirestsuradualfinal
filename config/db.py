ce74abe101a99bfb24de48dd1004e9ed41cf691d
Taniapino <tvpalaciospi@cesde.net>
2024-07-19 15:34:32 -0500

    se crea servidor sura dual final

--- config/db.py
+++ config/db.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine,MetaData
 
-motor=create_engine("mysql+pymysql://root@localhost:3310/leonardo")
+motor=create_engine("mysql+pymysql://root@localhost:3306/pruebafast")
 meta=MetaData()
 
 
